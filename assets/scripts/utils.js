9531e201c75c6df16887ea6fd4b3b6203e808937
Navdeep Sharma <navdeepisno1@gmail.com>
2022-07-05 16:52:11 +0530

    First commit

---
+++ assets/scripts/utils.js
@@ -0,0 +1,31 @@
+function startLoading() {
+
+}
+
+var viewportH = document.documentElement.clientHeight;
+var viewportW = document.documentElement.clientWidth;
+
+function startParallax(e) {
+    var x = e.pageX;
+    var y = e.pageY;
+
+    x = viewportW / 2 - x;
+    y = viewportH / 2 - y;
+
+    x /= 100;
+    y /= 100;
+
+    var imgSoldier1 = document.getElementById("img-soldier-1");
+    var imgSoldier2 = document.getElementById("img-soldier-2");
+    var imgBackground = document.getElementById("img-background");
+
+    imgSoldier1.style.transform = `translateX(${x}px) translateY(${y}px)`;
+
+    x /= 2;
+    y /= 2;
+    imgSoldier2.style.transform = `translateX(-${x}px) translateY(-${y}px) scale(0.5) rotateY(180deg)`;
+
+
+
+
+}
